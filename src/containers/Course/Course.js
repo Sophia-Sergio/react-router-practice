e32c6e687cec7cc0823c2064eaa5b2970fe7d2b8
Sophia-Sergio <san.storres@gmail.com>
2018-05-20 13:40:00 -0400

    Add nested curses

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,14 +1,44 @@
 import React, { Component } from "react";
+import { withRouter } from 'react-router-dom';
 
 class Course extends Component {
+
+
+  state = {
+    loadedCourse: null
+  }
+
+  componentDidMount(){
+    this.loadData();
+  }
+
+  componentDidUpdate(){
+    this.loadData();
+  }
+
+  loadData(){
+    const id = this.props.match.params.id;
+    if(id){
+      if (!this.state.loadedCourse || (this.state.loadedCourse && this.state.loadedCourse.id != id)){
+        console.log(this.props.course)
+        this.setState({ loadedCourse: this.props.course });
+        console.log(this.state)
+      }
+    }
+  }
+
   render() {
-    return (
-      <div>
-        <h1>_COURSE_TITLE_</h1>
-        <p>You selected the Course with ID: _ID_</p>
-      </div>
-    );
+    let course = <h5>loading</h5>;
+    if (this.state.loadedCourse){
+      course = (
+        <div>
+          <h1>{this.state.loadedCourse.title}</h1>
+          <p>You selected the Course with ID: _ID_</p>
+        </div>
+      )
+    }
+    return course;
   }
 }
 
-export default Course;
+export default withRouter(Course);
--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -1,30 +1,74 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link, Route } from "react-router-dom";
+import Course from "../Course/Course";
+import { withRouter } from 'react-router-dom';
 
-import './Courses.css';
+import "./Courses.css";
 
 class Courses extends Component {
-    state = {
-        courses: [
-            { id: 1, title: 'Angular - The Complete Guide' },
-            { id: 2, title: 'Vue - The Complete Guide' },
-            { id: 3, title: 'PWA - The Complete Guide' }
-        ]
+  state = {
+    courses: [
+      { id: 1, title: "Angular - The Complete Guide" },
+      { id: 2, title: "Vue - The Complete Guide" },
+      { id: 3, title: "PWA - The Complete Guide" }
+    ],
+    selectedCourse: null
+  };
+
+  componentDidMount(){
+    console.log(this.props)
+  }
+
+  // courseHandler(id) {
+  //   alert(id);
+  //   this.setState({
+  //     selectedCourse: id
+  //   })
+  // }
+
+  courseHandler = id => {
+    this.setState({
+      selectedCourse: id
+    });
+  };
+
+  render() {
+    let route =  <Route
+            path={this.props.match.url + "/:id"}/>;
+    if (this.state.selectedCourse) {
+      route = (
+        <Route
+          path={this.props.match.url + "/:id"}
+          render={() => (
+            <Course
+              course={this.state.courses[this.state.selectedCourse - 1]}
+            />
+          )}
+        />
+      );
     }
 
-    render () {
-        return (
-            <div>
-                <h1>Amazing Udemy Courses</h1>
-                <section className="Courses">
-                    {
-                        this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
-                        } )
-                    }
-                </section>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <h1>Amazing Udemy Courses</h1>
+        <section className="Courses">
+          {this.state.courses.map(course => {
+            return (
+              <Link
+                to={"/courses/" + course.id}
+                className="Course"
+                key={course.id}
+                onClick={() => this.courseHandler(course.id)}
+              >
+                {course.title}
+              </Link>
+            );
+          })}
+        </section>
+        {route}
+      </div>
+    );
+  }
 }
 
-export default Courses;
+export default withRouter(Courses);
